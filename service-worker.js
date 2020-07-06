35d8a28aeecbbdd2692f7affcfc87adc4029c957
Adir Dayan <adirdayan@gmail.com>
2020-07-06 14:15:06 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5bba662d82a6accedf79569e054396c.js"
+  "/precache-manifest.f607facfe80845745261eb7d3ec1f8eb.js"
 );
 
 self.addEventListener('message', (event) => {
