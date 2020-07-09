633e57abdbce2501a7e5e047f77f14dbec80d468
Adir Dayan <adirdayan@gmail.com>
2020-07-09 16:14:01 +0200

    contact page

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Contact.css';
 import axios from "axios";
 import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -28,7 +27,7 @@ class Contact extends React.Component {
     e.preventDefault();
     this.setState({ loading: true });
     try {
-      await axios.post('https://ovjkzf0d1j.execute-api.us-east-1.amazonaws.com/prod', { name: this.state.name, email: this.state.email, subject: this.state.subject, message: this.state.message })
+      await axios.post('https://ovjkzf0d1j.execute-api.us-east-1.amazonaws.com/prod', { first: this.state.first, last: this.state.last, email: this.state.email, mobile: this.state.mobile, subject: this.state.subject, message: this.state.message })
       setTimeout(() => {
         this.setState({ success: true, loading: false });
       }, 2000);
@@ -40,38 +39,69 @@ class Contact extends React.Component {
     }
   }
   render() {
+    console.log(this.state)
     return (
-      <React.Fragment>
-        <div className="content_inner contact">
-          <h1 className="contact-title">Let's stay in touch</h1>
+      <div className="contact-wrapper">
+        <div className="title-first">
           <br />
-          <h4 className="contact-text">For any further intrigues don't hesitate to contact me using the form below:</h4>
-          <br />
-          {!this.state.success && (
-            <form method="POST" onSubmit={e => this.handleSubmit(e)}>
-              <div className="wrapper">
-                <div className="container">
-                  <div className="row">
-                    <div className="form-group">
-                      <input onChange={e => this.handleChange(e)} name="name" type="text" id="name" className="form-control" required />
-                      <label htmlFor="name" className="ph-area">Name</label>
+          <h1>Contact Me</h1>
+          <br /><br />
+        </div>
+        <div className="contact-div">
+          <div className="container">
+            <div className="title-second">
+              <h4>For any further intrigues don't hesitate to<br />
+        contact me using the form below:</h4>
+            </div>
+            {!this.state.success && (
+              <form method="POST" onSubmit={e => this.handleSubmit(e)}>
+                <div className="row100">
+                  <div className="col">
+                    <div className="inputBox">
+                      <input onChange={e => this.handleChange(e)} type="text" name="first" required="required" />
+                      <span className="text">First Name *</span>
+                      <span className="line"></span>
                     </div>
-                    <div className="form-group">
-                      <input onChange={e => this.handleChange(e)} name="email" type="text" id="email" className="form-control" required />
-                      <label htmlFor="email" className="ph-area">Email</label>
+                  </div>
+                  <div className="col">
+                    <div className="inputBox">
+                      <input onChange={e => this.handleChange(e)} type="text" name="last" required="required" />
+                      <span className="text">Last Name *</span>
+                      <span className="line"></span>
                     </div>
-                    <div className="form-group">
-                      <input onChange={e => this.handleChange(e)} name="subject" type="text" id="subject" className="form-control" required />
-                      <label htmlFor="subject" className="ph-area">Subject</label>
+                  </div>
+                </div>
+                <div className="row100">
+                  <div className="col">
+                    <div className="inputBox">
+                      <input onChange={e => this.handleChange(e)} type="text" name="email" required="required" />
+                      <span className="text">Email *</span>
+                      <span className="line"></span>
                     </div>
-                    <div className="form-group">
-                      <textarea onChange={e => this.handleChange(e)} name="message" rows="5" type="text" id="message" className="form-control" required />
-                      <label htmlFor="message" className="ph-area">Message</label>
+                  </div>
+                  <div className="col">
+                    <div className="inputBox">
+                      <input onChange={e => this.handleChange(e)} type="text" name="mobile" required="required" />
+                      <span className="text">Mobile</span>
+                      <span className="line"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="row100">
+                  <div className="col">
+                    <div className="inputBox textarea">
+                      <textarea onChange={e => this.handleChange(e)} required="required" name="message"></textarea>
+                      <span className="text">Type your message Here... *</span>
+                      <span className="line"></span>
                     </div>
                   </div>
                 </div>
                 {!this.state.loading && (
-                  <button className="btn btn-info">Submit</button>
+                  <div className="row100">
+                    <div className="col">
+                      <input type="submit" value="Send" />
+                    </div>
+                  </div>
                 )}
                 {this.state.loading && (
                   <img
@@ -80,29 +110,29 @@ class Contact extends React.Component {
                     style={{ width: "50px" }}
                   />
                 )}
+              </form>
+            )}
+            {this.state.success && (
+              <div id="mailSent">
+                <img
+                  alt="success"
+                  style={{ width: "100px" }}
+                  src="./pictures/check.svg"
+                />
+                <h1>Your Email was sent successfully.</h1>
               </div>
-            </form>
-          )}
-          {this.state.success && (
-            <div id="mailSent">
-              <img
-                alt="success"
-                style={{ width: "100px" }}
-                src="./pictures/check.svg"
-              />
-              <h1>Your Email was sent successfully.</h1>
-            </div>
-          )}
-          {this.state.fail && (
-            <h1>Something went wrong, please try again.</h1>
-          )}
-          <br />
-          <div className="icons">
-            <a className="pointer" onClick={e => this.handleContact(e, 'github')}><i value="github" className="github icon fa fa-github fa-2x"></i></a>
-            <a className="pointer" onClick={e => this.handleContact(e, 'linkedin')}><i className="linkedin icon fa fa-linkedin fa-2x"></i></a>
+            )}
+            {this.state.fail && (
+              <h1>Something went wrong, please try again.</h1>
+            )}
           </div>
         </div>
-      </React.Fragment>
+        <br /><br /><br />
+        <div className="icons">
+          <a className="pointer" onClick={e => this.handleContact(e, 'github')}><i value="github" className="github icon fa fa-github fa-2x"></i></a>
+          <a className="pointer" onClick={e => this.handleContact(e, 'linkedin')}><i className="linkedin icon fa fa-linkedin fa-2x"></i></a>
+        </div>
+      </div>
     );
   }
 }
