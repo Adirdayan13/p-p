85f05e2fb985c77e47a9495069f70fe8d78e66d1
Adir Dayan <adirdayan@gmail.com>
2020-09-16 21:56:11 +0200

    Change domain

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
-import React from 'react';
-import './App.css';
-import './App.scss';
-import $ from 'jquery';
-import Contact from './Contact';
-import Projects from './Projects';
-import Home from './Home';
-import About from './About';
+import React from "react";
+import "./App.css";
+import "./App.scss";
+import $ from "jquery";
+import Contact from "./Contact";
+import Projects from "./Projects";
+import Home from "./Home";
+import About from "./About";
 
 const ref = React.createRef();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { home: true, projects: false, about: false, contact: false }
+    this.state = { home: true, projects: false, about: false, contact: false };
   }
 
   handleMenu() {
-    const $page = $('.page');
-    const $body = $('body');
-    const $menu_items = $('.menu_items');
-    const $content_inner = $('content_inner');
+    const $page = $(".page");
+    const $body = $("body");
+    const $menu_items = $(".menu_items");
+    const $content_inner = $("content_inner");
     ref.current.scrollIntoView({
-      block: 'start'
+      block: "start",
     });
-    $page.toggleClass('shazam');
-    $body.toggleClass('overflow noscroll');
+    $page.toggleClass("shazam");
+    $body.toggleClass("overflow noscroll");
     setTimeout(function () {
-      // $menu_items.addClass('hidden');
-      $menu_items.toggleClass('hidden');
+      $menu_items.toggleClass("hidden");
     }, 500);
 
-
-    $('.content').on('click', function () {
-      $page.removeClass('shazam');
-      $body.removeClass('noscroll');
-      $content_inner.removeClass('noscroll');
+    $(".content").on("click", function () {
+      $page.removeClass("shazam");
+      $body.removeClass("noscroll");
+      $content_inner.removeClass("noscroll");
       setTimeout(function () {
-        $menu_items.addClass('hidden');
+        $menu_items.addClass("hidden");
       }, 500);
     });
 
-    $('.menu_items').on('click', function () {
-      $page.removeClass('shazam');
-      $body.removeClass('overflow noscroll');
-      $content_inner.removeClass('noscroll');
+    $(".menu_items").on("click", function () {
+      $page.removeClass("shazam");
+      $body.removeClass("overflow noscroll");
+      $content_inner.removeClass("noscroll");
       setTimeout(function () {
-        // $menu_items.addClass('hidden');
-        $menu_items.addClass('hidden');
+        $menu_items.addClass("hidden");
       }, 500);
     });
   }
@@ -55,29 +52,77 @@ class App extends React.Component {
     return (
       <div className="App" ref={ref}>
         <div className="page">
-          <span className="menu_toggle menu-top" id="menu-top" onClick={this.handleMenu}>
+          <span
+            className="menu_toggle menu-top"
+            id="menu-top"
+            onClick={this.handleMenu}
+          >
             <i className="menu_open fa fa-bars fa-lg"></i>
             <i className="menu_close fa fa-times fa-lg"></i>
           </span>
           <ul className="menu_items hidden">
-            <li onClick={() => this.setState({ home: true, projects: false, about: false, contact: false })}><a><i className="icon fa fa-home fa-2x"></i> Home</a></li>
-            <li onClick={() => this.setState({ projects: true, home: false, about: false, contact: false })}><a><i className="icon fa fa-coffee fa-2x"></i> Projects</a></li>
-            <li onClick={() => this.setState({ about: true, home: false, projects: false, contact: false })}><a><i className="icon fa fa-user fa-2x"></i> About</a></li>
-            <li onClick={() => this.setState({ contact: true, home: false, projects: false, about: false })}><a><i className="icon fa fa-paper-plane fa-2x"></i> Contact</a></li>
+            <li
+              onClick={() =>
+                this.setState({
+                  home: true,
+                  projects: false,
+                  about: false,
+                  contact: false,
+                })
+              }
+            >
+              <a>
+                <i className="icon fa fa-home fa-2x"></i> Home
+              </a>
+            </li>
+            <li
+              onClick={() =>
+                this.setState({
+                  projects: true,
+                  home: false,
+                  about: false,
+                  contact: false,
+                })
+              }
+            >
+              <a>
+                <i className="icon fa fa-coffee fa-2x"></i> Projects
+              </a>
+            </li>
+            <li
+              onClick={() =>
+                this.setState({
+                  about: true,
+                  home: false,
+                  projects: false,
+                  contact: false,
+                })
+              }
+            >
+              <a>
+                <i className="icon fa fa-user fa-2x"></i> About
+              </a>
+            </li>
+            <li
+              onClick={() =>
+                this.setState({
+                  contact: true,
+                  home: false,
+                  projects: false,
+                  about: false,
+                })
+              }
+            >
+              <a>
+                <i className="icon fa fa-paper-plane fa-2x"></i> Contact
+              </a>
+            </li>
           </ul>
           <main className="content">
-            {this.state.home && (
-              <Home />
-            )}
-            {this.state.projects && (
-              <Projects />
-            )}
-            {this.state.about && (
-              <About />
-            )}
-            {this.state.contact && (
-              <Contact />
-            )}
+            {this.state.home && <Home />}
+            {this.state.projects && <Projects />}
+            {this.state.about && <About />}
+            {this.state.contact && <Contact />}
           </main>
         </div>
       </div>
